1ee238308a2a6e69905eedf796ff1b2a7bd01d6e
Muhammad Zair <mhammadzair@gmail.com>
2024-12-02 00:27:23 +0500

    trying to fix the cors problem

--- src/main/java/com/server/HealthNet/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/server/HealthNet/SecurityConfig/SecurityConfig.java
@@ -31,7 +31,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.disable())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers(
